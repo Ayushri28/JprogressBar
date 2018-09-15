f525f3323e0afe6f9f67ccb34545b5dd590b0a72
Ayushri28 <42199863+Ayushri28@users.noreply.github.com>
2018-09-15 18:52:59 +0530

    updating program

--- sample.java
+++ sample.java
@@ -4,14 +4,10 @@ System.out.println("Enter range");
 Scanner input=new Scanner(System.in);
 int n=input.nextInt();
 
-
 for(int i=0; i<n; i++)
 {
-
   String str=input.next();
-  System.out.println(str + " ");
-  
+  System.out.println(str + " ");  
 }
-
 
 }

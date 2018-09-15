8cecea74a2e2eaa796c9229f942ae9ead9cab5b5
Ayushri28 <42199863+Ayushri28@users.noreply.github.com>
2018-09-15 18:45:49 +0530

    creating sample program

---
+++ sample.java
@@ -0,0 +1,17 @@
+public class Main{
+
+System.out.println("Enter range");
+Scanner input=new Scanner(System.in);
+int n=input.nextInt();
+
+
+for(int i=0; i<n; i++)
+{
+
+  String str=input.next();
+  System.out.println(str + " ");
+  
+}
+
+
+}
